f9b5f3115de49860a98125203ec1a05c35c451fb
Daniel H <frontend@reddesignsystems.com>
2016-06-01 17:58:53 -0500

    read file async

---
+++ index.js
@@ -0,0 +1,25 @@
+var fs = require('fs')
+
+/*creando nuestro propio read file async - receta para hacer funciones asincronicas*/
+function readFileText (name, cb) {
+	/*llamar a la funcion nextTick que esta dentro de process, esta funcion recibe como  parametro otra function
+	y lo que pasa aqui es que process.nextTick es que coge esta function que le paso por parametro y la va a 
+	mandar a la cola de eventos para diferir su ejecucion y va a seguir con la proxima instruccion, es decir 
+	process.nextTick() agarra mi function y la pone en la cola de eventos hasta que el levenloop decida que 
+	la funcion termino y llame al callback */
+	process.nextTick(function () {
+		var content = fs.readFileSync(name)
+		cb(content.toString())
+	})
+}
+
+readFileText('./lorem.txt', function (content) {
+	console.log(content)
+})
+console.log('Hola Platzi')
+
+var reader = new TextReader('./lorem.txt')
+
+reader.on('end', function () {
+	
+})
